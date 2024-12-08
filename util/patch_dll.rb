885a319b6f054ab6f2363871b1c23a46e59c31d9
edelkas <edlucasma@gmail.com>
2024-12-08 03:23:44 +0100

    better manual dll patcher

--- util/patch_dll.rb
+++ util/patch_dll.rb
@@ -1,11 +1,22 @@
-HOST = "https://dojo.nplusplus.ninja"
-PROXY = "45.32.150.168:8126/ctp".ljust(HOST.length, "\x00")
+TEST  = true
+PACK  = 'ctp'
+HOST  = "https://dojo.nplusplus.ninja"
+IP    = TEST ? '127.0.0.1' : '45.32.150.168'
+PORT  = 8126
+PROXY = "#{IP}:#{PORT}/#{PACK}".ljust(HOST.length, "\x00")
+IN    = 'npp.dll'
+OUT   = 'npp.dll'
 
-fn = ARGV[0] || 'npp.dll'
-raise "#{fn} not found in folder" if !File.file?(fn)
-file = File.binread(fn)
-file.include?(HOST) ? file.gsub!(HOST, PROXY) : (
-  file.include?(PROXY) ? file.gsub!(PROXY, HOST) : (raise "URL not found")
-)
-File.binwrite(fn + 'P', file)
-
+raise "#{IN} not found in folder" if !File.file?(IN)
+file = File.binread(IN)
+test = TEST ? ' (test)' : ''
+if file.include?(HOST)
+  file.gsub!(HOST, PROXY)
+  puts "Patched #{PACK.upcase}#{test}"
+elsif file.include?(PROXY)
+  file.gsub!(PROXY, HOST)
+  puts "Unpatched #{PACK.upcase}#{test}"
+else
+  raise "URL#{test} not found"
+end
+File.binwrite(OUT, file)
